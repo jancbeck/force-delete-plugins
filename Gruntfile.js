00f91e616403fa032c53b6d7f77dd34e47866772
Jan Beck <delite.jcb@googlemail.com>
2015-08-03 00:56:30 +0200

    added grunt-wp-plugin

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,1 +1,19 @@
-grunt.loadNpmTasks( 'grunt-wp-plugin' );
+module.exports = function(grunt) {
+	
+	grunt.initConfig({
+	  wp_plugin: {
+	    deploy: {
+	      options: {
+	        assets_dir: 'assets',   // Relative path to your assets directory (optional).
+	        deploy_dir: 'build',   // Relative path to your deploy directory (required).
+	        plugin_slug: 'force-delete-plugins',
+	        svn_username: 'jancbeck'
+	      }
+	    }
+	  }
+	});
+
+	grunt.loadNpmTasks( 'grunt-wp-plugin' );
+
+};
+
